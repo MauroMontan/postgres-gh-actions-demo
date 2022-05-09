bf75fec32474b18d8a5906ec4aa81650e2415691
Mauro <mauro@pop-os.localdomain>
2022-05-09 17:40:57 -0500

    adding posts endpoint

---
+++ test/post_model.test.js
@@ -0,0 +1,17 @@
+
+import Post from "./app/models/post_model.js";
+
+
+describe("test suit for post model",()=>{
+    test("1) instantiate post model",()=>{                
+    const data = new Post({
+        title: "post title",
+        author: "random author",
+        authorId: 1,
+        content: "content",
+        published: false
+    });
+
+        expect(data).toBe(instanceof Post );
+    });
+});
