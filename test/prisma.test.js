9245d86be6b6a302c9d702f238b26a920e0122a2
Mauro <mauro@pop-os.localdomain>
2022-05-09 18:30:05 -0500

    adding prisma test

--- test/prisma.test.js
+++ test/prisma.test.js
@@ -1,1 +1,22 @@
+import Prisma from "@prisma/client";
 
+const prisma = new Prisma.PrismaClient();
+
+describe("test suit for prisma", () => {
+
+    test("1) get post from database", async () => {
+        const data = {
+            id:1,
+            title:"Hello",
+            content:null,
+            published:false,
+            author:null,
+            authorId:null
+        };
+        const post = await prisma.post.findUnique({
+            where:{id:1},
+        }); 
+
+        expect(post).toEqual(data);
+    });
+});
